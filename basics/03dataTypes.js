9dae1b17c47cb9cfdbaeb37503cc98539e03b4e7
Akash Jha <126976034+Akashjha248@users.noreply.github.com>
2023-09-24 19:47:38 +0000

    Datatypess

---
+++ basics/03dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert(3 + 3) //We are using nodejs, not the browser
+//alert runs in the browser or index.html file nad not in nodejs
+
+let name = "Akash"        //String
+let age = 21             //Integer
+let isLoggedIn = true   //Boolean
+
+/*Various Datatypes
+1. Number
+2. bigint
+3. string = ""
+4. boolean
+5. null -> standalone value
+6. undefined
+7. symbol -> to find uniqueness
+*/
+
+console.log(typeof "Akash"); //type of "Akash" is string
