531eb044bd271f0a23745c34d10c4fbad08a7ed0
Akash Jha <126976034+Akashjha248@users.noreply.github.com>
2023-10-05 18:05:07 +0000

    strings

---
+++ basics/07strings.js
@@ -0,0 +1,19 @@
+//console.log("hello" + " world");
+
+const name = "Akash"
+const age = 22
+
+//console.log(`My name is ${name} and I am ${age} years old`);
+
+console.log(name.length);
+console.log(name[0]);
+console.log(name.toUpperCase());
+console.log(name.charAt(1));
+console.log(name.indexOf('t'));
+
+const newName = "     Akash     "
+console.log(newName);
+console.log(newName.trim())
+
+const url = "https://akash.com/akash%20placement";
+console.log(url.replace('%20', '--'));
