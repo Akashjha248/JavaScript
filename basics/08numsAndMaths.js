b8d6828060689e63d8205807029d4ec351540bc6
Akash Jha <126976034+Akashjha248@users.noreply.github.com>
2023-10-06 19:35:41 +0000

    NumsAndMaths

---
+++ basics/08numsAndMaths.js
@@ -0,0 +1,22 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+
+//const otherNumber = 1000000
+//console.log(otherNumber.toLocaleString('en-IN'));
+
+//console.log(Math);
+
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6));
+
+//console.log(Math.random() * 10);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
